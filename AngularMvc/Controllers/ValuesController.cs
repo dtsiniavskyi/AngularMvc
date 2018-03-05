b1f7dce59ceac2e7a496a44b0864e220c99b4987
Dmytro <dmytro.tsiniavskyi@gmail.com>
2018-03-05 14:19:15 +0200

    moved values controller to Controllers folder

--- AngularMvc/Controllers/ValuesController.cs
+++ AngularMvc/Controllers/ValuesController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AngularMvc
+namespace AngularMvc.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
