ff0e01c06ae82a667889fcd19707018276bff64b
Dmytro <dmytro.tsiniavskyi@gmail.com>
2018-03-05 12:27:44 +0200

    changed HttpModule to HttpClientModule

--- AngularMvc/src/app/app.module.ts
+++ AngularMvc/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,7 @@ import { ValuesService } from './services/values.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpModule
+    HttpClientModule
   ],
   providers: [ValuesService],
   bootstrap: [AppComponent]
