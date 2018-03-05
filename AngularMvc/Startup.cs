8d251967e55d75080719ffdb756c5c2a75e38b2e
Dmytro <dmytro.tsiniavskyi@gmail.com>
2018-03-05 15:38:51 +0200

    added more comments to startup.cs

--- AngularMvc/Startup.cs
+++ AngularMvc/Startup.cs
@@ -7,16 +7,20 @@ namespace AngularMvc
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime.
+        // Use this method to add services to the conteiner.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime.
+        // Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Redirect any non-API calls to the Angular application
+            // so our application can handle the routing
             app.Use(async (context, next) => {
                 await next();
 
@@ -31,8 +35,12 @@ namespace AngularMvc
                 }
             });
 
+            // Configure application for usage as API
+            // with default route of '/api/[Controller]'
             app.UseMvcWithDefaultRoute();
 
+            // Configures application to serve the index.html file from /wwwroot
+            // when you access the server from a browser
             app.UseDefaultFiles();
             app.UseStaticFiles();
         }
--- AngularMvc/src/app/app.module.ts
+++ AngularMvc/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HomeComponent } from './components/home/home.component';
 import { ValuesService } from './services/values.service';
 
