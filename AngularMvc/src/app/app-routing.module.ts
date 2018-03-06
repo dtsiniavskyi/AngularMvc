adcd68fb32679df2d3a9844085cc2564326537b1
Dmytro <dmytro.tsiniavskyi@gmail.com>
2018-03-06 21:18:28 +0200

    added home and counter components, implemented navigation

--- AngularMvc/src/app/app-routing.module.ts
+++ AngularMvc/src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { DataComponent } from './components/data/data.component';
+import { CounterComponent } from './components/counter/counter.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'counter', component: CounterComponent },
+  { path: 'data', component: DataComponent },
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
--- AngularMvc/src/app/app.module.ts
+++ AngularMvc/src/app/app.module.ts
@@ -10,13 +10,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ValuesService } from './services/values.service';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DataComponent } from './components/data/data.component';
+import { CounterComponent } from './components/counter/counter.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    DataComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
